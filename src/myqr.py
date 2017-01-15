de23421ee52dbff850582c75940a3e9217ea5467
Aaron Thomas <1d20m6@gmail.com>
2017-01-14 21:53:21 -0500

    Stashing is scary, so I'm doing this

--- src/myqr.py
+++ src/myqr.py
@@ -10,7 +10,7 @@ from mathutil import *
 NOTE:
 color-tuples are 3 element tuples structred as (R,G,B), 0-255 each
 
-point-tuples are 2 element tuples structed as (x,y)
+point-tuples are 2 element tuples structed as (x,y) @todo(someone) make a point class or
 (note that this is consistent with pillows use of points)
 vectors are equivalent to point-tuples, but with a different context
 cluster-tuple is a (point-tuple, int) where the int is cluster size
@@ -196,30 +196,83 @@ def scanImage(image):
 	returns a list of tuple-points which are the centers of any QR code corner identifiers
 	'''
 	#lineclusters will be a list of lists (one per horizontal line) of cluster-tuples
+	print(image.size[1])
 	lineclusters = [getPixelClusters(image, (0,i), (1,0)) for i in range(image.size[1])]
-
+	lineclusters = []
+	for i in range(image.size[1]):
+		lineclusters.append(getPixelClusters(image, (0,i), (1,0)))
 	iclusters = [] #interesting clusters
 	for line in lineclusters:
 		matches = []
+		print("line{}".format(line))
+		if len(line) < 5:
+			iclusters.append(matches)
+			continue
+
 		for i in range(len(lineclusters)-4): #scan the clusters five at a time to find the QR pattern
-			scanthis = line[i:i+5]			 #we're looking for 1-1-3-1-1
+			scanthis = line[i:i+5]	 #we're looking for 1-1-3-1-1
+			#print(scanthis)
+			if len(scanthis) < 5:
+			 	continue
 			baselen = len(scanthis[0])
 			if kindaEquals(baselen, scanthis[1][1]) and kindaEquals(baselen, scanthis[3][1]) and kindaEquals(baselen, scanthis[4][1]) and kindaEquals(baselen*3, scanthis[2][1]): #i'm so sorry for this line of code. basically it just checks for the 1 1 3 1 1 pattern in qr code corners
-				matches.append(scanthis[3][1]/2 + scanthis[3][1][0]) #adds the middle of the middle of the scan to the matches
+				matches.append(scanthis[3][1][0]/2 + scanthis[3][1][0]) #adds the middle of the middle of the scan to the matches
 		iclusters.append(matches)
-
+	#print(iclusters)
 	cluster_points = [] #this is a list of cluster points that we will be returning
 	current_line = -1 #we start at -1 because i do +1 at the beginning
-	while current_line < image.size[1]:
+	while current_line < image.size[1]-1:
 		current_line = current_line + 1
-		if len(lineclusters[current_line]) < 0:
+		if len(iclusters[current_line]) < 0:
 			continue #so that i can just do this lazy line of code
-		for current_col in lineclusters[current_line]: #this for loop handles when there's two clusters in one row (bottom of QR code)
+		scanline = current_line
+		for current_col in iclusters[current_line]: #this for loop handles when there's two clusters in one row (bottom of QR code)
 			scanline = current_line #we sometimes need to scanline down twice
-			while len(lineclusters[scanline] > 0) and kindaEquals(lineclusters[scanline], current_line): #make sure that the clusters match up
+			while len(iclusters[scanline] > 0) and kindaEquals(iclusters[scanline], current_line): #make sure that the clusters match up
 				scanline = scanline + 1
 			cluster_center = (current_col, int((scanline - current_line)/2)) #current_col is the position in the row
 			cluster_points.append(cluster_center)							#scanline is end of the cluster, current_line is beginning so we can take their mean to get the middle
 		current_line = scanline #after scanning the clusters we set current line to scanline
 									#if we get lots of very nearby points change previous line to blabla = scanline + 1
 	return cluster_points
+
+	def simplifyPoints(points):
+		'''
+		@param
+			points is an iterable containing many point-tuples
+		ok so like. find clusters of points. points are always gonna be basically in the same line
+		this is gonna be a bad code
+		im sorry future me who has to maybe delete this
+		i'll do lots of comments (i hope)
+		'''
+		#pass #@todo(aaronn) implement this. goal is to have it working by 1am
+		'''
+		LMAO OK
+		so the plan is to make "clusters" of colinear ones
+		then we break up the clusters if there's blank space
+		then we kill any cluster that's really small
+		then we can just take the mean of the points
+		'''
+		'''
+		ok so the structure of one of these clusters im just gonn make a class
+		'''
+		clusters = []
+		prevpoint = points[0]
+		for i in range(1,len(points)):
+			newcluster = LineCluster(prevpoint,points[i])
+			clusters.append(newline) if not all(newcluster.equals(cluster) for cluster in clusters) or len(clusters) == 0
+			for point in points[i:-1]:
+
+class LineCluster:
+	def __init__(self,p1,p2):
+	self.members = [p1,p2]
+	self.line = Line(p1,p2)
+
+	def add(point):
+		members.append(point) if line.contains(point)
+
+    def size():
+    	return len(members)
+
+    def equals(cluster):
+    	return self.line.equals(cluster.line)
